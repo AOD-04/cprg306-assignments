5af1756cc2419b9f0f5bbab88ac2cc08ac714abe
AOD-04 <159972187+AOD-04@users.noreply.github.com>
2025-01-28 23:09:33 -0700

    Part 2
    Part 2 of the assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>
+        Anton Tan
+      </p>
+      <Link href="https://github.com/AOD-04/cprg306-assignments">
+        My Github
+      </Link>
+    </div>
+  );
+}
